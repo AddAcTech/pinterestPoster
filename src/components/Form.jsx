13225325333c340386e82a62b3a615c8f924d0f3
AddAcTech <aguilar.chavez.alexis.daniel@gmail.com>
2023-12-02 21:29:44 -0600

    Screenshot, form styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,35 +1,31 @@
 import React from "react";
+import * as htmlToImage from "html-to-image";
+
+let node = document.getElementById("poster");
 
 function Form() {
   return (
-    <div className="flex flex-col bg-neutral-500 m-4 p-4 rounded-lg shadow-lg shadow-neutral-600">
-      <label className="my-1">Title:</label>
-      <input
-        type="text"
-        className="rounded-lg px-2"
-        placeholder="Poster Title"
-      />
-
-      <label className="my-1">Author:</label>
-      <input type="text" className="rounded-lg px-2" placeholder="Author" />
-
-      <label className="my-1">Year:</label>
-      <input
-        type="text"
-        className="rounded-lg px-2"
-        placeholder="Poster year"
-      />
-
-      <label className="my-1">URL de la Imagen:</label>
-      <input
-        type="text"
-        className="rounded-lg px-2"
-        placeholder="Poster Image"
-      />
-
-      <div className="my-2 flex flex-col text-white font-bold">
-        <button className="bg-zinc-950  rounded-md my-2">Generar Póster</button>
-        <button className="bg-orange-500 rounded-md">Descargar Póster</button>
+    <div className="flex flex-col bg-[#add8e6] m-4 p-4 rounded-lg border-black border-solid border-2 shadow-[5px_5px_0_0_rgba(0,0,0,1)]">
+      <p className="text-2xl font-bold text-[#323232]">Bienvenido,</p>
+      <p className="text-[#636363] font-semibold">crea tu poster.</p>
+      <input type="text" className="input" placeholder="Poster Title" />
+      <input type="text" className="input" placeholder="Author" />
+      <input type="text" className="input" placeholder="Poster year" />
+      <input type="text" className="input" placeholder="Poster Image" />
+      <div className="my-2 flex gap-2">
+        <button
+          className="input"
+          onClick={() =>
+            htmlToImage.toPng(node).then(function (dataUrl) {
+              let img = new Image();
+              img.src = dataUrl;
+              document.body.appendChild(img);
+            })
+          }
+        >
+          Generar Poster
+        </button>
+        <button className="input">Descargar Poster</button>
       </div>
     </div>
   );
--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import bat from "../assets/bat.jpg";
+import * as htmlToImage from "html-to-image";
 
 function Poster() {
+  const node = useRef(null);
+
   return (
     <div>
-      <div className="bg-[#f0e68c] p-[25px] w-[450px]">
+      <div ref={node} className="bg-[#dcd9d2] p-[25px] w-[450px]" id="poster">
         <div className="w-[400px] h-[400px] bg-[#ffe4c4] flex justify-center items-center mx-auto overflow-hidden object-contain mb-3">
           <img
             className="object-cover overflow-hidden"
             id="poster-image"
-            src="default-image.jpg"
+            src={bat}
             alt="Poster Image"
           />
         </div>
-        <h2 className="font-bold text-2xl" id="poster-title">
-          Poster title
-        </h2>
-        <p className="font-bold text-xl" id="poster-description">
-          Author
-        </p>
-        <p className="font-bold" id="poster-year">
-          Poster Year
-        </p>
+        <div>
+          <span className="font-bold text-2xl" id="poster-title">
+            THE BATMAN
+          </span>
+          <span className="font-bold ml-2" id="poster-year">
+            2021
+          </span>
+        </div>
+        <div>
+          <span className="font-bold text-xl" id="poster-description">
+            DIRECTED BY
+          </span>
+          <span className="font-bold text-xl ml-2" id="poster-year">
+            Matt Reeves
+          </span>
+        </div>
       </div>
+      <button
+        className="input"
+        onClick={() =>
+          htmlToImage
+            .toJpeg(node.current, { quality: 0.95 })
+            .then(function (dataUrl) {
+              let link = document.createElement("a");
+              link.download = "myPoster.jpeg";
+              link.href = dataUrl;
+              link.click();
+            })
+        }
+      >
+        Generar -{">"}
+      </button>
     </div>
   );
 }
